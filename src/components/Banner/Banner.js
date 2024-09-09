cbd2864918230e7fb2c4d08ea1c2185dd5aecb2f
João Pedro <jpsbat56@gmail.com>
2024-09-09 19:13:06 -0300

    feat: banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <header class="banner">
+      <img src="/images/banner.png" alt="Banner principal" />;
+    </header>
+  );
+}
+
+export default Banner;
